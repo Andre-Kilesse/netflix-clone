cc7872bce695aec2bf3fbe17bec7aaa4f7cef8df
Andre-Kilesse <kilesseandre@gmail.com>
2025-12-11 21:27:27 -0300

    commit inicial

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { ENV_VARS } from "./envVars.js";
+
+export const connectDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(ENV_VARS.MONGO_URI);
+
+        console.log(`MongoDB connected: ${connection.host}`);
+    } catch (error) {
+        console.error("Error connection to MongoDB: ", error.message);
+        process.exit(1);
+    }
+}
