50740c8e3275d51ecc4c7453cdf7273fbdbd78ed
Andre-Kilesse <kilesseandre@gmail.com>
2025-12-18 00:28:33 -0300

    feat: Add logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,7 +61,7 @@ export async function signup(req, res) {
     }
     catch (error) {
         console.log("Error in signup controller", error.message);
-        res.stats(400).json({ succes: false, message: "Internal server error" })
+        res.status(400).json({ succes: false, message: "Internal server error" })
     }
 }
 
@@ -70,5 +70,11 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
-    res.send('Logout route');
+    try {
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({ succes: true, message: "Logged out" })
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
 }
