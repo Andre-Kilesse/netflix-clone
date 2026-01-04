79dfe84f09cbafcc2d1d7e518ea511b2a32e4a1a
andre@kilesse <kilesseandre@gmail.com>
2026-01-04 02:15:56 -0300

    feat:add JWT route protection middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import { protectRoute } from './middleware/protectRoute.js';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -11,9 +14,11 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/movie', movieRoutes);
+app.use('/api/v1/movie', protectRoute, movieRoutes);
+app.use('/api/v1/tv', protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
     console.log('Server listen at http://localhost:' + PORT);
