eef96d592e8002e75fb743f94e8ec5e2fc9b4492
andre@kilesse <kilesseandre@gmail.com>
2026-01-06 01:26:00 -0300

    add signup searchParams to mail

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 
 const SignUpPage = () => {
-  const [email, setEmail] = useState("");
+  const [searchParams] = useSearchParams();
+  const emailValue = searchParams.get("email");
+  const [email, setEmail] = useState(emailValue || "");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  
   const handleSignUp = (e) => {
     e.preventDefault();
     console.log(email, username, password)
